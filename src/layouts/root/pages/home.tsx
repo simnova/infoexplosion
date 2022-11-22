3d494e70e471bf17e8de880a9d081112fc14bb81
Patrick Gidich <patrick.gidich@simnova.com>
2022-11-22 00:36:22 -0500

    fufu

--- src/layouts/root/pages/home.tsx
+++ src/layouts/root/pages/home.tsx
@@ -1,7 +1,39 @@
+import styles from './home.module.css'; 
 import { ReactComponent as Crest } from '../../../assets/13-crest.svg';
-import { ReactComponent as StarburstGold } from '../../../assets/00-starburst-gold-on-center-no-bounding-box.svg';
 import { ReactComponent as MuseumOfInformationExplosionText } from '../../../assets/25-museum-of-information-explosion-text.svg';
 import { ReactComponent as MieLogo } from '../../../assets/14-mie-logo.svg';
+
+import { ReactComponent as BarRight } from '../../../assets/08-bar-right.svg';
+import { ReactComponent as BarLeft } from '../../../assets/09-bar-left.svg';
+import { ReactComponent as RibbonBehindCrest } from '../../../assets/10-ribbon-behind-crest.svg';
+import { ReactComponent as RightRibbonEnd } from '../../../assets/12-right-ribbon-end.svg';
+import { ReactComponent as LeftRibbonEnd } from '../../../assets/11-left-ribbon-end.svg';
+
+import { ReactComponent as RibbonFrontOfCrest } from '../../../assets/15-ribbon-front-of-crest.svg';
+import { ReactComponent as LightningBoltsTop } from '../../../assets/16-lightening-bolts-top.svg';
+import { ReactComponent as LightningBoltsBottomLeft } from '../../../assets/17-lightening-bolts-bottom-left.svg';
+import { ReactComponent as LightningBoltsBottomRight } from '../../../assets/18-lightening-bolts-bottom-right.svg';
+
+
+import { ReactComponent as MorseCodeM1 } from '../../../assets/26-morse-code-m1.svg';
+import { ReactComponent as MorseCodeM2 } from '../../../assets/27-morse-code-m2.svg';
+import { ReactComponent as MorseCodeI1 } from '../../../assets/28-morse-code-i1.svg';
+import { ReactComponent as MorseCodeI2 } from '../../../assets/29-morse-code-i2.svg';
+import { ReactComponent as MorseCodeE } from '../../../assets/30-morse-code-e.svg';
+
+
+
+
+
+
+import { ReactComponent as ScrollRight } from '../../../assets/06-scroll-right.svg';
+import { ReactComponent as ScrollLeft } from '../../../assets/07-scroll-left.svg';
+
+
+import { ReactComponent as StarburstBlue } from '../../../assets/04-starburst-blue.svg';
+import { ReactComponent as StarburstGold } from '../../../assets/00-starburst-gold-on-center-no-bounding-box.svg';
+
+
 import { useScroll } from 'framer-motion';
 import { useRef, useEffect, useState, useLayoutEffect } from 'react';
 import { useResizeDetector } from 'react-resize-detector';
@@ -56,10 +88,31 @@ export const Home: React.FC<any> = (_props) => {
         <div style={{ height: '200px', backgroundColor: 'blue', padding: '10px', color: 'white' }}>Hello</div>
         <div ref={parentRef} style={{ minHeight: '400px', maxHeight:'800px', backgroundColor: '#b3c8f1', padding: '10px', color: 'white', overflow:'hidden' }}>
           <div ref={ref}  style={{maxWidth: `${width}px`, margin: '0 auto', position:'relative' }}>
-              <MuseumOfInformationExplosionText style={{position:'absolute', stroke:'white', }} />
-              <MieLogo style={{position:'absolute' }} />
-              <Crest style={{position:'static', zIndex:2, strokeOpacity:0,opacity:`${vvalue===0?0:100},` }} />
-              <StarburstGold style={{position:'absolute', strokeOpacity:0, opacity:`${vvalue}`,transform:`translateX(-${backgroundLeftOffset}px)`, margin:'0 auto', top:`-${topOffset}px`, zIndex:0, minWidth:`${parentWidth}px`}} />
+              <MuseumOfInformationExplosionText style={{position:'absolute',zIndex:3  }} />
+              <MieLogo style={{position:'absolute' ,zIndex:3}} />
+              <BarLeft style={{position:'absolute',zIndex:3 }} />
+              <BarRight style={{position:'absolute',zIndex:3}} />
+              <RibbonBehindCrest style={{position:'absolute',zIndex:3}} />
+              <RightRibbonEnd style={{position:'absolute',zIndex:3}} />
+              <LeftRibbonEnd style={{position:'absolute',zIndex:3}} />
+              <RibbonFrontOfCrest style={{position:'absolute',zIndex:3}} />
+              <LightningBoltsTop style={{position:'absolute',zIndex:3}} />
+              <LightningBoltsBottomLeft style={{position:'absolute',zIndex:3}} />
+              <LightningBoltsBottomRight style={{position:'absolute',zIndex:3}} />
+              <MorseCodeM1 className={styles['morsecodem1']} style={{position:'absolute',zIndex:3}} />
+              <MorseCodeM2 className={styles['morsecodem2']} style={{position:'absolute',zIndex:3}} />
+              <MorseCodeI1 className={styles['morsecodei1']} style={{position:'absolute',zIndex:3}} />
+              <MorseCodeI2 className={styles['morsecodei2']} style={{position:'absolute',zIndex:3}} />
+              <MorseCodeE className={styles['morsecodee']} style={{position:'absolute',zIndex:3}} />
+
+
+              <ScrollLeft style={{position:'absolute',zIndex:3, left: '0px', top: '0px' }} />
+              <ScrollRight style={{position:'absolute',zIndex:3, right: '0px', top: '0px' }} />
+
+              <Crest style={{position:'static', zIndex:2, opacity:'100', visibility:`${vvalue===0?'hidden':'inherit'}` }} />
+              <StarburstBlue style={{position:'absolute', zIndex:1,  opacity:`${vvalue}`,transform:`translateX(-${backgroundLeftOffset}px)`, margin:'0 auto', top:`-${topOffset}px`,  minWidth:`${parentWidth}px`}} />
+              <div className={[ styles['whitePulse']].join(' ')} style={{position:'absolute', zIndex:1,  opacity:`${vvalue}`, margin:'0 auto',  left:`0px`, top:`-${width/12}px`, minWidth:`${width}px`, minHeight:`${width}px`}} />
+              <StarburstGold style={{position:'absolute', zIndex:0,  opacity:`${vvalue}`,transform:`translateX(-${backgroundLeftOffset}px)`, margin:'0 auto', top:`-${topOffset}px`,  minWidth:`${parentWidth}px`}} />
 
 
 
