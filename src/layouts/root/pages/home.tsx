be4d4f5947fb4776482420a47f1a1d374863182a
Patrick Gidich <patrick.gidich@simnova.com>
2022-11-22 16:42:23 -0500

    scrolling

--- src/layouts/root/pages/home.tsx
+++ src/layouts/root/pages/home.tsx
@@ -23,10 +23,15 @@ import { ReactComponent as MorseCodeE } from '../../../assets/30-morse-code-e.sv
 
 import { ReactComponent as NavBar } from '../../../assets/nav/nav-00-bar.svg';
 import { ReactComponent as NavAbout } from '../../../assets/nav/nav-01-about.svg';
+import { ReactComponent as NavAboutHover } from '../../../assets/nav/nav-01b-about-over.svg';
 import { ReactComponent as NavAttractions } from '../../../assets/nav/nav-02-attractions.svg';
+import { ReactComponent as NavAttractionsHover } from '../../../assets/nav/nav-02b-attractions-over.svg';
 import { ReactComponent as NavGetInvolved } from '../../../assets/nav/nav-03-get-involved.svg';
+import { ReactComponent as NavGetInvolvedHover } from '../../../assets/nav/nav-03b-get-involved-over.svg';
 import { ReactComponent as NavNewsAndEvents } from '../../../assets/nav/nav-04-news-and-events.svg';
+import { ReactComponent as NavNewsAndEventsHover } from '../../../assets/nav/nav-04b-news-and-events-over.svg';
 import { ReactComponent as NavSupportTheMuseum } from '../../../assets/nav/nav-05-support-the-museum.svg';
+import { ReactComponent as NavSupportTheMuseumHover } from '../../../assets/nav/nav-05b-support-the-museum-over.svg';
 import { ReactComponent as NavVisit } from '../../../assets/nav/nav-99-visit.svg';
 
 
@@ -42,20 +47,29 @@ import { ReactComponent as StarburstBlue } from '../../../assets/04-starburst-bl
 import { ReactComponent as StarburstGold } from '../../../assets/00-starburst-gold-on-center-no-bounding-box.svg';
 
 
-import { useScroll } from 'framer-motion';
+import { useScroll, useInView } from 'framer-motion';
 import { useRef, useEffect, useState, useLayoutEffect } from 'react';
 import { useResizeDetector } from 'react-resize-detector';
-import { Z_BINARY } from 'zlib';
 
 export const Home: React.FC<any> = (_props) => {
   const ref = useRef(null);
   const parentRef = useRef(null);
+
+  const aboutRef = useRef(null);
+  const attractionsRef = useRef(null);
+  const getInvolvedRef = useRef(null);
+  const newsAndEventsRef = useRef(null);
+  const supportTheMuseumRef = useRef(null);
+
+
   const { width:parentWidth, height:parentHeight } = useResizeDetector({ targetRef: parentRef });
   const { width:childWidth, height:childHeight } = useResizeDetector({ targetRef: ref });
 
   const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+  const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
   const [vvalue, setvvalue] = useState(100);
-  const maxWidth = parentWidth??0; // Math.min(vw * 0.8, 1000);
+  const maxWidth = parentWidth??(Math.min(vw * 0.8, 1000));
+  console.log('maxWidth', maxWidth);
   const [width, setWidth] = useState(maxWidth);
   //const [parentWidth, setParentWidth] = useState(0);
 
@@ -88,14 +102,39 @@ export const Home: React.FC<any> = (_props) => {
   console.log("width",childWidth);
   let backgroundLeftOffset =   ((parentWidth??0)/2) + ((childWidth??0)/2) ; // (width > (600) ? ((parentWidth??0/2) - (width/2)) : (parentWidth??0/2 - width)/2);
   let topOffset = (parentWidth??0) > (childWidth??0 )?  ((parentWidth??0)/2)-((childWidth??0)/2): 0;// (parentHeight??0)/2;
+
+
+  const aboutInView = useInView(aboutRef,{
+    margin: `-50% 0px -50% 0px`
+  })
+  const attractionsInView = useInView(attractionsRef,{
+    margin: `-50% 0px -50% 0px`
+  })
+  const getInvolvedInView = useInView(getInvolvedRef,{
+    margin: `-50% 0px -50% 0px`
+  })
+  const newsAndEventsInView = useInView(newsAndEventsRef,{
+    margin: `-50% 0px -50% 0px`
+  })
+  const supportTheMuseumInView = useInView(supportTheMuseumRef,{
+    margin: `-50% 0px -50% 0px`
+  })
+
+
   return (
     <div style={{margin:'0 auto',maxWidth:1000}}>
       <div style={{  backgroundColor: 'yellow', margin: '0 auto', position:'sticky', top:'0px', zIndex:3 }}>
-            <NavAbout style={{position:'absolute',zIndex:3}} />
-            <NavAttractions style={{position:'absolute',zIndex:3}} />
-            <NavGetInvolved style={{position:'absolute',zIndex:3}} />
-            <NavNewsAndEvents style={{position:'absolute',zIndex:3}} />
-            <NavSupportTheMuseum style={{position:'absolute',zIndex:3}} />
+            <NavAbout style={{position:'absolute',zIndex:3,display:aboutInView?'none':'initial'}} />
+            <NavAboutHover style={{position:'absolute',zIndex:2,display:!aboutInView?'none':'initial'}} />
+            <NavAttractions style={{position:'absolute',zIndex:3,display:attractionsInView?'none':'initial'}} />
+            <NavAttractionsHover style={{position:'absolute',zIndex:2,display:!attractionsInView?'none':'initial'}} />
+            <NavGetInvolved style={{position:'absolute',zIndex:3,display:getInvolvedInView?'none':'initial'}} />
+            <NavGetInvolvedHover style={{position:'absolute',zIndex:2,display:!getInvolvedInView?'none':'initial'}} />
+            <NavNewsAndEvents style={{position:'absolute',zIndex:3,display:newsAndEventsInView?'none':'initial'}} />
+            <NavNewsAndEventsHover style={{position:'absolute',zIndex:2,display:!newsAndEventsInView?'none':'initial'}} />
+            <NavSupportTheMuseum style={{position:'absolute',zIndex:3,display:supportTheMuseumInView?'none':'initial'}} />
+            <NavSupportTheMuseumHover style={{position:'absolute',zIndex:2,display:!supportTheMuseumInView?'none':'initial'}} />
+
             
             <NavBar style={{position:'absolute'}} />
             <Crest style={{position:'absolute',left:'50%',transform:'translate(-50%,-28%)',maxWidth: `${targetWidth}px`,display: (vvalue === 0?'block':'none')}} />
@@ -136,10 +175,11 @@ export const Home: React.FC<any> = (_props) => {
 
         </div>
         Hello World! {scrollYProgress.get()} -width: {width}px -vvalue: {vvalue}
-        <div style={{ height: '200px', backgroundColor: 'blue', padding: '10px', color: 'white' }}>Hello</div>
-        <div style={{ height: '200px', backgroundColor: 'blue', padding: '10px', color: 'white' }}>Hello</div>
-        <div style={{ height: '200px', backgroundColor: 'blue', padding: '10px', color: 'white' }}>Hello</div>
-        <div style={{ height: '200px', backgroundColor: 'blue', padding: '10px', color: 'white' }}>Hello</div>
+        <div ref={aboutRef} style={{ height: '500px', backgroundColor: 'blue', padding: '10px', color: 'white' }}>About</div>
+        <div ref={attractionsRef} style={{ height: '500px', backgroundColor: 'green', padding: '10px', color: 'white' }}>Attractions</div>
+        <div ref={getInvolvedRef} style={{ height: '500px', backgroundColor: 'orange', padding: '10px', color: 'white' }}>Get Involved</div>
+        <div ref={newsAndEventsRef} style={{ height: '500px', backgroundColor: 'blue', padding: '10px', color: 'white' }}>News & Events</div>
+        <div ref={supportTheMuseumRef} style={{ height: '500px', backgroundColor: 'green', padding: '10px', color: 'white' }}>Support the Museum</div>
       </div>
     </div>
   );
